030c4c58750689d8718055c66b946d7c25720877
PavelK6896 <79208776896@yandex.ru>
2022-06-09 22:37:27 +0300

    add build 1

---
+++ build1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+deployBuild="https://localhost:8081"
+
+appName="10317-read2-ts-angular"
+search="http://localhost:8080"
+files="./dist/$appName/*"
+
+echo 'Build 1'
+npm run "build prod static"
+
+for f in $files; do
+  if [[ $search != "" && $deployBuild != "" && "$f" == *"main"* ]]; then
+    sed -i "s+$search+$deployBuild+g" "$f"
+  fi
+done
+
+echo 'Build 1 ok'
+exit
